da88d2ebaa6a1b22730951a14f85d85cde226ec2
boogiu <boogiww@gmail.com>
2024-04-13 11:33:41 +0900

    비디오 태그 안에 중복되는 소스 속성 제거

--- src/View/Home.jsx
+++ src/View/Home.jsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 function Home() {
     return (
       <HomeConatainer>
-        <VideoPlayer muted autoPlay loop playsinline>
-            <source src={process.env.PUBLIC_URL + '/VideoSrc/Example.mp4'} type="video/mp4" />
+        <VideoPlayer muted autoPlay loop playsinlinesrc={process.env.PUBLIC_URL + '/VideoSrc/Example.mp4'} >
         </VideoPlayer>
       </HomeConatainer>
     );
